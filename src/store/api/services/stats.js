9d12bb4f44b70b5bb503b16a984fe88ee849699e
James Daily <jamespdaily@outlook.com>
2017-07-22 23:58:14 -0400

    major data store and login updates

--- src/store/api/services/stats.js
+++ src/store/api/services/stats.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default {
   getAllStats (request = {}) {
-    return axios.get('http://localhost:3000/api/Player_Stats/getPlayerStats?year=All-time&orderBy=full_name ASC', request)
+    return axios.get('https://shielded-cliffs-33205.herokuapp.com/api/Player_Stats/getPlayerStats?year=All-time&orderBy=full_name ASC', request)
       .then((response) => Promise.resolve(response))
       .catch((error) => Promise.reject(error))
   }
